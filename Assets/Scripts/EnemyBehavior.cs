c75511c73146629d6998fb9db33851353c0e898d
Unknown <bob.van.unen@hotmail.com>
2018-09-04 15:56:29 +0200

    Merge

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    public float speed;
+    Renderer m_Renderer;
+   
+
+    // Use this for initialization
+    void Start () {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    
+
+    // Update is called once per frame
+    void Update () {
+
+        transform.position += new Vector3 (0, -speed, 0) * Time.deltaTime;
+        Vector3 a = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        
+        if (transform.position.y < a.y)
+        {
+            Destroy(gameObject);
+                        
+        }
+
+    }
+}
