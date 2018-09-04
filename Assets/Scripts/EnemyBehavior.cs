0da00570855b1860b30b8e86334b8c09ac80eb63
Chiel van Wonderen <cvwonderen@gmail.com>
2018-09-04 16:27:44 +0200

    Merge fixes

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -6,20 +6,14 @@ public class EnemyBehavior : MonoBehaviour {
 
     public float speed;
     Renderer m_Renderer;
-<<<<<<< HEAD
+
     private SpawnSystem spawner;   
 
     // Use this for initialization
     void Start () {
         spawner = GameObject.Find("Spawner").GetComponent<SpawnSystem>();
         spawner.enemies.Add(gameObject);
-=======
-   
 
-    // Use this for initialization
-    void Start () {
->>>>>>> 6a8b46f5b4e559ce1e89a74213704cf569c8cff3
-        m_Renderer = GetComponent<Renderer>();
     }
 
     
@@ -32,10 +26,9 @@ public class EnemyBehavior : MonoBehaviour {
         
         if (transform.position.y < a.y)
         {
-<<<<<<< HEAD
+
             spawner.enemies.Remove(gameObject);
-=======
->>>>>>> 6a8b46f5b4e559ce1e89a74213704cf569c8cff3
+
             Destroy(gameObject);
                         
         }
