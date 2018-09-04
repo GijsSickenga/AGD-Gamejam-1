df92dbc54c67d8f9327378e2acf384ca7ac8d08f
Chiel van Wonderen <cvwonderen@gmail.com>
2018-09-04 14:01:44 +0200

    First test version, enemies and spawning

---
+++ Assets/SpawnSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSystem : MonoBehaviour {
+
+    public GameObject[] spawners;
+    public GameObject enemy;
+    private Vector3 spawnPoint;
+
+    public float spawnRate;
+
+    // Use this for initialization
+    void Start () {
+        Spawn();
+    }
+	
+
+    public IEnumerator Spawn()
+    {
+        float f = 1 / spawnRate;
+        yield return new WaitForSeconds(f);
+        spawnPoint = spawners[Random.Range(0, 4)].transform.position;
+        GameObject.Instantiate(enemy, spawnPoint, Quaternion.identity);
+        StartCoroutine(Spawn());
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Spawn());
+            
+        }
+        
+        
+    }
+}
