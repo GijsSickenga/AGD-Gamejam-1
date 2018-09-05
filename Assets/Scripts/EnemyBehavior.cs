fbb5c30f54e3c549332aeefed7745e80cb6fa6d5
Yannick <mulderyannick@gmail.com>
2018-09-05 10:51:21 +0200

    Okee

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -21,10 +21,10 @@ public class EnemyBehavior : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
-        transform.position += new Vector3 (0, -speed, 0) * Time.deltaTime;
+        transform.position += new Vector3 (speed, 0, 0) * Time.deltaTime;
         Vector3 a = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
         
-        if (transform.position.y < a.y)
+        if (transform.position.x < a.x)
         {
 
             spawner.enemies.Remove(gameObject);
--- Assets/Scripts/SpawnSystem.cs
+++ Assets/Scripts/SpawnSystem.cs
@@ -8,6 +8,10 @@ public class SpawnSystem : MonoBehaviour {
     public GameObject enemy;
     private Vector3 spawnPoint;
 
+    float spawnTime = 5f;
+
+    float spawnTimePassed = Time.time ;
+
     public float spawnRate;
 
     public List <GameObject> enemies;
@@ -24,17 +28,23 @@ public class SpawnSystem : MonoBehaviour {
         float f = 1 / spawnRate;
         yield return new WaitForSeconds(f);
         spawnPoint = spawners[Random.Range(0, 4)].transform.position;
-        GameObject.Instantiate(enemy, spawnPoint, Quaternion.identity);
+        GameObject.Instantiate(enemy, spawnPoint, Quaternion.Euler(0, 0, 90));
         StartCoroutine(Spawn());
     }
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (spawnTime < spawnTimePassed)
         {
             StartCoroutine(Spawn());
-            
+            spawnTime += 5;
         }
-        
+
+        else
+
+        {
+            spawnTime--;
+        }
         
     }
 }
